62a201ba3f41848cb75ca6604b376174bc685a7a
jjesswan <jessica_wan@brown.edu>
2026-05-20 22:23:44 -0400

    toon shader outline info

--- src/content/toonshader_content.tsx
+++ src/content/toonshader_content.tsx
@@ -11,19 +11,24 @@ const info: WorkProps[] = [
     image: "/images/ToonShader/steps.gif"
   },
   {
-    header: `1 + 2. Shadow + Highlight Banding`,
+    header: `Shadow + Highlight Banding`,
     desc: "Uses light direction from level blueprint during runtime to calculate the angle between the surface normal and light source. This angle is fed into multiple value bins (i.e. shadow, midtones, highlight), which can be adjusted to determine where intermediate values fall.",
     image: "/images/ToonShader/binning.png"
   },
   {
-    header: `3. Rim Lighting`,
+    header: `Rim Lighting`,
     desc: "Creates alpha mask with transition falloff between hard shadow and hard light, with parameters to control rim width and softness",
     image: "/images/ToonShader/rimlight.png"
   },
   {
-    header: `4-6. Applying Light Models to Input Material`,
+    header: `Applying Light Models to Input Material`,
     desc: "Takes either solid color or texture input and applies the toon shading model on top, allowing it to be used across a wide range of materials. Highlight, shadow, and rim lighting color each have their own color and brightness parameter controls.",
     image: "/images/ToonShader/applying_light.png"
+  },
+  {
+    header: `Outline Overlay Material`,
+    desc: "Applies inverted hull technique to create a colored outline around the object. The outline thickness is adjustable, and the material can be toggled on or off to allow for dynamic control during gameplay.",
+    image: "/images/ToonShader/demo_outline.gif"
   },
 ]
 
